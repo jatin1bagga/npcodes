bac847a7aa960b82bdcbf71b8f1b821d1f71598d
jatinb <68025499+jatin1bagga@users.noreply.github.com>
2024-12-02 00:24:20 +0530

    Update multicast_rec.c

--- NP/asgt7/multicast_rec.c
+++ NP/asgt7/multicast_rec.c
@@ -1,66 +1,70 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
-struct sockaddr_in sockaddr;
-struct ip_mreq group;
-int sd;
-int datalen;
-char databuf[1024];
+#define MULTICAST_GROUP "239.0.0.1"  // Multicast group address (Class D)
+#define MULTICAST_PORT 12345         // Port to receive the message
+#define BUFFER_SIZE 256
 
-int main (int argc, char *argv[]){
-    sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sd < 0) {
-        perror("opening datagram socket");
-        exit(1);
-    }
-
+int main() {
+    int sockfd;
+    struct sockaddr_in local_addr;
+    struct ip_mreq multicast_request;
+    char buffer[BUFFER_SIZE];
     int reuse = 1;
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
-        perror("setting SO_REUSEADDR");
-        close(sd);
-        exit(1);
+
+    // 1. Create a UDP socket (AF_INET, SOCK_DGRAM)
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    memset((char *) &sockaddr, 0, sizeof(sockaddr));
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port = htons(5555);
-    sockaddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sd, (struct sockaddr*)&sockaddr, sizeof(sockaddr))) {
-        perror("binding datagram socket");
-        close(sd);
-        exit(1);
+    // 2. Set SO_REUSEADDR option to allow multiple receivers on the same port
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        perror("Setting SO_REUSEADDR failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
 
-    group.imr_multiaddr.s_addr = inet_addr("225.1.1.1"); 
-    group.imr_interface.s_addr = inet_addr("127.0.0.1");  
+    // 3. Initialize the sockaddr_in structure (local address and port)
+    memset(&local_addr, 0, sizeof(local_addr));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // Receive datagrams for any local IP
+    local_addr.sin_port = htons(MULTICAST_PORT);     // Port to receive the multicast
 
-    if (setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0) {
-        perror("adding multicast group");
-        close(sd);
-        exit(1);
+    // Bind the socket to the local address and port
+    if (bind(sockfd, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
 
+    // 4. Use IP_ADD_MEMBERSHIP to join the multicast group
+    multicast_request.imr_multiaddr.s_addr = inet_addr(MULTICAST_GROUP);  // Multicast group
+    multicast_request.imr_interface.s_addr = htonl(INADDR_ANY);           // Local interface
+
+    if (setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &multicast_request, sizeof(multicast_request)) < 0) {
+        perror("Adding to multicast group failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // 5. Receive the multicast message
     while (1) {
-        datalen = sizeof(databuf);
-        if (read(sd, databuf, datalen) < 0) {
-            perror("reading datagram message");
-            close(sd);
-            exit(1);
+        int nbytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, 0);
+        if (nbytes < 0) {
+            perror("Receive failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
         }
-        printf("Received : %s", databuf);
-        close(sd);
-        exit(1);
+
+        buffer[nbytes] = '\0';  // Null-terminate the received message
+        printf("Received multicast message: %s\n", buffer);
     }
 
-    setsockopt(sd, IPPROTO_IP, IP_DROP_MEMBERSHIP, (char *)&group, sizeof(group));
-    close(sd);
-
+    close(sockfd);
     return 0;
 }
